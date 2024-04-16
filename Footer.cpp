f597add9fd2eed4ed5c286f5cd8e563e8f47b5ac
J1a2n3s4 <jedrzejburczynski@gmail.com>
2024-04-16 12:02:37 +0200

    stopka dziala ez

--- Footer.cpp
+++ Footer.cpp
@@ -1,21 +1,23 @@
 #include "Footer.h"
-
+#include <iostream>
 
 
 Footer::Footer()
 {
-	BG.setFillColor(sf::Color(20, 20, 20));
+	BG.setFillColor(sf::Color(50, 50, 50));
 	BG.setOutlineColor(sf::Color(200, 200, 200));
-	BG.setOutlineThickness(2);
+	BG.setOutlineThickness(1);
 	size.x = 1000;
-	size.y = 100;
+	size.y = 200;
 	BG.setSize(sf::Vector2f(1000,100));
 	BG.setPosition(0,1000);
 }
 
 void Footer::Process(sf::RenderWindow* WSK, sf::Vector2i Mouse, sf::Vector2f WinSize)
 {
-	BG.setScale(1000/WinSize.x,1000/WinSize.y);
-	BG.setOrigin(0,size.y/WinSize.y);
+	BG.setPosition(1,WinSize.y);
+	BG.setScale(999/WinSize.x,1000/WinSize.y);
+	BG.setSize(sf::Vector2f(WinSize.x-2,100));
+	BG.setOrigin(0,size.y*(1000/WinSize.y)/2);
 	WSK->draw(BG);
 }
